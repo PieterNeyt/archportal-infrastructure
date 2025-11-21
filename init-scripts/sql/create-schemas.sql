36ca90d76dde1e0b2036015aff7927c1e63e3dab
Cian Van Acker <cian.vanacker@student.kdg.be>
2025-11-22 00:09:41 +0100

    docker compose

---
+++ init-scripts/sql/create-schemas.sql
@@ -0,0 +1,6 @@
+create schema gameservice;
+create schema analyticsservice;
+create schema lobbyservice;
+create schema shopservice;
+create schema communicationservice;
+create schema profileservice;
